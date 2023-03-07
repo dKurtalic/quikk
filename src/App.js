af778a6ec5e7ae02587d00982889402f90e34ca4
Dina Kurtalic <dkurtalic1@etf.unsa.ba>
2023-03-07 21:53:05 +0100

    Zapocet searchbar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Box } from '@mui/material';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Navbar, SearchFeed, VideoDetail, ChannelDetail, Feed } from './components';
+
+function App() {
+    return (
+
+        <BrowserRouter>
+            <Box sx={{ backgroundColor: '#000' }}>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Feed />} />
+                    <Route path="/video/:id" element={<VideoDetail />} />
+                    <Route path="/search/:searchTerm" element={<SearchFeed />} />
+                    <Route path="/channel/:id" element={<ChannelDetail />} />
+                </Routes>
+            </Box>
+        </BrowserRouter>
+
+    );
+}
+
+
+export default App
---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoDetail = () => {
+    return (
+        <h1>VideoDetail</h1>
+    )
+}
+
+export default VideoDetail
