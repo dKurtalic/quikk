790addb37df01fde754ba074386d30e2195c5c99
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-03-08 02:18:41 +0100

    Zapocet rad s APIjem

---
+++ src/utils/fetch.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
+const options = {
+    method: 'GET',
+    url: BASE_URL,
+    params: { part: 'snippet', videoId: 'M7FIvfx5J10' },
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};
+
+export const fetch = async ({ url }) => {
+    await axios.get(`${BASE_URL}`)
+}
