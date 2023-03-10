e5745235877c45f954879088d88092db71f940fd
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-03-10 23:37:32 +0100

    env ne radi

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,26 +1,20 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
 const options = {
 
-    params: { part: 'snippet', videoId: 'M7FIvfx5J10' },
+    params: {
+        maxResults: '50'
+    },
     headers: {
         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
+    },
 };
 
 export const fetch = async (url) => {
-    console.log("Ovo je url " + url);
-    try {
-        const response = await axios.get(`${BASE_URL}/${url}`, options);
-        console.log("evo response " + response);
-        const data = JSON.stringify(response)
-        console.log("JSON " + data)
-        return { response }
-    } catch (error) {
-        console.log("o joj");
-        throw error;
-    }
-
+    console.log("api key " + process.env.REACT_APP_RAPID_API_KEY)
+    const { response } = await axios.get(`${BASE_URL}/${url}`, options);
+    return response;
 }
