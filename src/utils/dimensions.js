3f3db2999777e019d18c8ad90afde0fb2aebf969
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-03-16 00:57:32 +0100

    Završen Video detail, mali refactoring

---
+++ src/utils/dimensions.js
@@ -0,0 +1,12 @@
+export const SPACING = {
+    small: '8px',
+    sm: '12px',
+    medium: '16px',
+    large: '24px',
+};
+
+export const FONT_SIZE = {
+    small: '12px',
+    medium: '16px',
+    large: '24px',
+};
